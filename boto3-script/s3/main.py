baece25fc0d81d8e31d844f6aa52b0cadb4768d9
sudovazid <vazidmail@protonmail.com>
2024-12-21 21:16:14 +0530

    Added: S3 script

--- boto3-script/s3/main.py
+++ boto3-script/s3/main.py
@@ -1,28 +1,12 @@
 import logging
 import boto3
-import botocode.exceptions import ClientError
 
-def create_bucket(bucket_name, region=None):
-    try:
-        if region is None:
-            s3_client = boto3.client('s3')
-            s3_client.create_bucket(Bucket=bucket_name)
-        else:
-            s3_client = boto3.client('s3', region_name=region)
-            location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
-    except ClientError as e:
-        logging.error(e)
-        return False
+s3 = boto3.resource('s3')
 
 def list_bucket():
-    s3 = boto3.client('s3')
-    response = s3.list_bucket()
-
     print('Existing buckets:')
-    for bucket in response['Buckets']:
-        print(f'{bucket["Name"]'}
+    for bucket in s3.buckets.all():
+        print(f"Name: {bucket.name}")
 
 
-list_bucket()
-print()
+print(list_bucket())
